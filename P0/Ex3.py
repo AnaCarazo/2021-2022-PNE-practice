4abb02a261aee7aae6463c0a8e31e7586a228eb6
acarazol <ana.carazo.lacunzaa@gmail.com>
2022-02-09 18:41:57 +0100

    po

---
+++ P0/Ex3.py
@@ -0,0 +1,5 @@
+import Seq0
+list_seq = ["ADA", "FRAT", "FXN", "RNU6_269P", "U5"]
+FOLDER =
+for l in list_dna:
+    print("Gene", l, "---> Length:", len(seq0.seq_read_fasta("./seq_dna/" + l + ".txt")))
