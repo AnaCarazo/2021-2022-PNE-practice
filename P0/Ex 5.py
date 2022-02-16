5e6effa48361619091715da2985cfe8633aada72
acarazol <ana.carazo.lacunzaa@gmail.com>
2022-02-16 18:15:02 +0100

    CHANGE

--- P0/Ex 5.py
+++ P0/Ex 5.py
@@ -2,6 +2,6 @@ import Seq0
 list_seq = ["U5", "ADA", "FRAT1", "FXN"]
 FOLDER = "./seq_dna/"
 for l in list_seq:
-    seq = (seq0.seq_read_fasta(FOLDER + l + ".txt")
+    seq = (Seq0.seq_read_fasta(FOLDER + l + ".txt")
     dict_seq = seq_count(seq)
     print("\nGene", l + ":", dict_seq)
--- P0/Ex3.py
+++ P0/Ex3.py
@@ -1,5 +1,5 @@
 import Seq0
-list_seq = ["ADA", "FRAT", "FXN", "RNU6_269P", "U5"]
-FOLDER = "./seq_dna/"
+list_seq = ["ADA", "FRAT1", "FXN", "RNU6_269P", "U5"]
+#FOLDER = "./seq_dna/"
 for l in list_seq:
-    print("Gene", l, "---> Length:", len(seq0.seq_read_fasta(FOLDER + l + ".txt")))
+    print("Gene", l, "---> Length:", len(Seq0.seq_read_fasta(l)))
