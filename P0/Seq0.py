ed730ec0e05f4e1e471150c925d2ab5f3c367a8b
Ana <ana.carazo.lacunzaa@gmail.com>
2022-02-22 10:57:10 +0100

    from S6

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -11,16 +11,25 @@ def valid_filename():
             return filename
         except FileNotFoundError:
             print("This file does not exist.")
+
 def seq_read_fasta(filename):
         seq = open("./seq_dna/" + filename + ".txt", "r").read()
         seq = seq[seq.find("\n") + 1:].replace("\n", "")
         return seq
+
+def seq_len(seq):
+    l = len(seq)
+    return l
 
 def seq_count(seq):
     list_count = [seq.count("A"), seq.count("C"), seq.count("T"), seq.count("G")]
     list_basis = ["A", "C", "T", "G"]
     dict_list = dict(zip(list_basis, list_count))
     return dict_list
+
+def seq_count_base(seq, A):
+    c = seq.count(A)
+    return c
 
 def seq_reverse(seq):
     frag = seq[:20]
