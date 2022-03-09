ff2e6473f335153d4d54c1f61aa54fc85116b5b1
acarazol <ana.carazo.lacunzaa@gmail.com>
2022-03-09 18:44:25 +0100

    8-3-2022

--- P3/server.py
+++ P3/server.py
@@ -31,25 +31,59 @@ while True:
     try:
         (cs, client_ip_port) = ls.accept()
 
-    # -- Server stopped manually
     except KeyboardInterrupt:
         print("Server stopped by the user")
-
-        # -- Close the listenning socket
         ls.close()
-
-        # -- Exit!
         exit()
 
     # -- Execute this part if there are no errors
     else:
         print("A client has connected to the server!")
         msg_raw = cs.recv(2048)
-        msg = msg_raw.decode()
+        msg = msg_raw.decode() #msg = msg_raw.decode().replace("\n", "").strip() : en windows, a veces nos llega el mensaje con un end of line character y con espacios, lo quitamos.
+        #del mensaje que nos llega necesritamos separarlo en dos partes, el command y el argumento
+        splitted_command = msg.split(" ") #para el PING command, que solo tiene un elemento, necesitamos calcular el argumento solo en el caso de que el command no sea ping
+        command = splitted_command[0]
+        #argument = splitted_command[1]
+        print(command)
+        if command != "PING":
+            argument = splitted_command[1]
+        print(command)
         print(f"Message received: {msg}")
-        if msg == "PING":
+
+#poner lo del mensaje en color verde, con lo de colorama
+        if command == "PING":
             response = "OK!\n"
+            print("PING COMMAND")
+
+        elif command == "GET":
+            list_seqs = ["ACTGACTG, CCAACCTTGG, GGAACCTTGGCCAATT, TGCATGCA, AACCTTGG"]
+            print(list_seqs[int(argument)])
+
+        elif command == "INFO":
+            from Seq1 import Seq
+            sequence = Seq(argument)
+            list_basis = ["A", "C", "T", "G"]
+            print("Sequence:", sequence.__str__(), "\nTotal length:", sequence.len(), "\n", str(list_basis[0]) + ":", sequence.count_base()[0], "(" + (sequence.count_base()[0]) * 100 / sequence.len(), ")", "\n", str(list_basis[1]) + ":", sequence.count_base()[1], "(" + (sequence.count_base()[1]) * 100 / sequence.len(), ")", "\n", str(list_basis[2]) + ":", sequence.count_base()[2], "(" + (sequence.count_base()[2]) * 100 / sequence.len(), ")", "\n", str(list_basis[3]) + ":", sequence.count_base()[3], "(" + (sequence.count_base()[3]) * 100 / sequence.len(), ")"
+
+        elif command == "COMP":
+            from Seq1 import Seq
+            sequence = Seq(argument)
+            print(sequence.complementary_seq())
+
+        elif command == "REV":
+            from Seq1 import Seq
+            sequence = Seq(argument)
+            print(sequence.seq_reverse())
+        elif command == "GENE":
+            from Seq1 import Seq
+            # -- Create a Null sequence
+            s = Seq()
+            # -- Initialize the null seq with the given file in fasta format
+            print(s.seq_read_fasta(argument))
+
         else:
             response = "This command is not available in the server\n"
+
         cs.send(response.encode())
         cs.close()
