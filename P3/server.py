efb2ee18a80baebad3894a17f324d56afbbeea7f
acarazol <ana.carazo.lacunzaa@gmail.com>
2022-03-09 17:20:02 +0100

    8-3-2022

---
+++ P3/server.py
@@ -0,0 +1,55 @@
+import socket
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "127.0.0.1"
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
+
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+    # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listenning socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    # -- Execute this part if there are no errors
+    else:
+        print("A client has connected to the server!")
+        msg_raw = cs.recv(2048)
+        msg = msg_raw.decode()
+        print(f"Message received: {msg}")
+        if msg == "PING":
+            response = "OK!\n"
+        else:
+            response = "This command is not available in the server\n"
+        cs.send(response.encode())
+        cs.close()
