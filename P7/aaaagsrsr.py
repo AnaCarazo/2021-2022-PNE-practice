ec2734da6c806112286dc3af21d94c8c11d54bb3
AnaCarazo <a.carazol.2021@alumnos.urjc.es>
2022-04-29 20:57:29 +0200

    29/04/2022

--- P7/aaaagsrsr.py
+++
@@ -1,7 +0,0 @@
-from Seq1 import Seq
-s = Seq("ACCTTTGGGG")
-dict_b_p = s.bases_and_percentages()
-print("dict: ", dict_b_p)
-list_basis = ["A", "C", "T", "G"]
-for i, b in enumerate(list_basis):
-    print("percentage of", b,  ":", str(list(dict_b_p.values())[i][1]) + "%")
